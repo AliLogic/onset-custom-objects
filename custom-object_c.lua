91f628e1e5b0651798efd92841def6506a1981cd
AliLogic <22447370+AliLogic@users.noreply.github.com>
2020-03-24 21:37:24 +0500

    Update custom-object_c.lua

--- custom-object_c.lua
+++ custom-object_c.lua
@@ -2,7 +2,7 @@ AddEvent("OnObjectStreamIn", function (objectid)
 	local mesh = GetObjectPropertyValue(object, "mesh");
 		
 	if mesh ~= nil then
-		GetObjectActor(objectid):SetObjectSkeletalMesh(mesh);
+		GetObjectActor(objectid):SetSkeletalMesh(USkeletalMesh.LoadFromAsset(mesh));
 			
 		if IsGameDevMode() then
 			AddPlayerChat("Streamed in a custom object with mesh " .. mesh);
