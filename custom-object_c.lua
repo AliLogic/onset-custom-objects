bda929b16a56260cd8bbc022c6df027758cd7d4d
AliLogic <22447370+AliLogic@users.noreply.github.com>
2020-03-24 23:21:32 +0500

    Add table method instead of property-values

--- custom-object_c.lua
+++ custom-object_c.lua
@@ -1,11 +1,19 @@
+local ObjectModels = {}
+
+function AddCustomModel(modelid, mesh)
+	table.insert(ObjectModels, modelid, mesh)
+end
+
 AddEvent("OnObjectStreamIn", function (objectid)
-	local mesh = GetObjectPropertyValue(object, "mesh");
+	local mesh = ObjectModels[objectid]
 		
 	if mesh ~= nil then
 		GetObjectActor(objectid):SetSkeletalMesh(USkeletalMesh.LoadFromAsset(mesh));
-			
+		
 		if IsGameDevMode() then
 			AddPlayerChat("Streamed in a custom object with mesh " .. mesh);
 		end
 	end
 end)
+
+AddFunctionExport("AddCustomModel", AddCustomModel)
